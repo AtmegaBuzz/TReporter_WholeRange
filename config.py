160b45a038d998c2b45ab591db1822714beb6e1e
swapnil shinde <creepydazze@gmail.com>
2022-01-07 12:50:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,3 +10,6 @@ spread_user_cred_json_file_addr = "cred.json"
 #input spred url containing links
 fetch_spreadsheet_url = "https://docs.google.com/spreadsheets/d/1bZqx3NSs1wxojqMqOAm7Pk-555j4-_C6zD2k9a22Pd8/edit#gid=0"
 
+starting_row = 1
+
+# usernames text without @ symbol
